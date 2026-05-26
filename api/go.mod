2b2b0b81d80128284102b97a6c73728eff2f9adf
Matteo Brambilla <matteo.brambilla@quantyca.it>
2026-05-26 15:03:15 +0200

    Setup api application

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module mrtutor-api
+
+go 1.26.3
---
+++ api/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Starting mrtutor backend server...")
+	fmt.Println("Servers shutdown complete")
+}
